6413dda8214b2da2c1aec09a7ecaab4fdf4fc07c
Thomas Chia <thomas.chia@amaas.com>
2018-01-18 23:05:06 +0800

    Add additional tracks

--- src/components/NewTrack/index.js
+++ src/components/NewTrack/index.js
@@ -6,9 +6,12 @@ export class Track extends React.Component {
   constructor() {
     super()
     this.state = {
-      duration: 0
+      duration: 0,
+      error: ''
     }
     this.handleDurationChange = this.handleDurationChange.bind(this)
+    this.handleError = this.handleError.bind(this)
+    this.handleOnCanPlay = this.handleOnCanPlay.bind(this)
   }
   componentDidMount() {
     this.props.registerTrack(this.props.id, this.audioNode)
@@ -24,8 +27,17 @@ export class Track extends React.Component {
   handleDurationChange(e) {
     this.setState({ duration: this.audioNode.duration })
   }
+  handleError() {
+    this.setState({ error: 'Error loading track' })
+  }
+  handleOnCanPlay() {
+    this.setState({ error: '' })
+  }
   render() {
-    return (
+    const { state } = this
+    return state.error ? (
+      <div style={styles.error}>{state.error}</div>
+    ) : (
       <div
         style={styles.addedTrack(
           this.state.duration / this.props.maxDuration * 100
@@ -33,10 +45,12 @@ export class Track extends React.Component {
       >
         <div style={styles.trackName}>{this.props.name}</div>
         <audio
+          onCanPlay={this.handleOnCanPlay}
           onDurationChange={this.handleDurationChange}
           muted={this.props.muted || this.props.masterMuted}
           ref={node => (this.audioNode = node)}
           src={this.props.src}
+          onError={this.handleError}
         >
           Audio not supported in this browser
         </audio>
@@ -60,5 +74,19 @@ export const styles = {
     width: `${width}%`,
     borderRadius: 10,
     border: 'solid 1px green'
-  })
+  }),
+  loading: {
+    display: 'flex',
+    alignItems: 'center',
+    height: '100%',
+    padding: '0 20px',
+    color: 'lightgreen'
+  },
+  error: {
+    display: 'flex',
+    alignItems: 'center',
+    height: '100%',
+    padding: '0 20px',
+    backgroundColor: 'pink'
+  }
 }
